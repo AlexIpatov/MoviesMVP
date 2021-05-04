8aa5e9151a7de194759b294dfd4ba5f4d9a272c6
AlexIpatov <65159241+AlexIpatov@users.noreply.github.com>
2021-05-04 12:33:34 +0300

    init

---
+++ MoviesMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesMVP
+//
+//  Created by Александр Ипатов on 04.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoviesMVPTests/MoviesMVPTests.swift
@@ -0,0 +1,33 @@
+//
+//  MoviesMVPTests.swift
+//  MoviesMVPTests
+//
+//  Created by Александр Ипатов on 04.05.2021.
+//
+
+import XCTest
+@testable import MoviesMVP
+
+class MoviesMVPTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
